047ca3ed668c521e7c1ea84be71e80d306908d3d
Francklin509 <deziusf@gmail.com>
2025-06-01 16:25:28 -0400

    llk

--- Nova_Plan_App/settings.py
+++ Nova_Plan_App/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600)
-}
 
 
 

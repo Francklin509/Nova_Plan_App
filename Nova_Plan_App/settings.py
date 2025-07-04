d363d4db998e556dd41264ec73773c65f6cc86a7
Francklin509 <162865584+Francklin509@users.noreply.github.com>
2025-07-04 11:15:51 -0400

    Update settings.py

--- Nova_Plan_App/settings.py
+++ Nova_Plan_App/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jp!n#4mq!oaq=0&+_k$$_5aj-!d6j9g-@l-%ivp4=&ikfjl1ty'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://nova-plan-builders.onrender.com', 'localhost', '127.0.0.1']
 

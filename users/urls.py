64e9e3363a2327537af61b80edc5e5e10ed606f7
Francklin509 <deziusf@gmail.com>
2025-07-04 10:24:51 -0400

    The last Commit001

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.registration),
-    path('login/', views.login),
+    path('', views.user_login),
+    path('registration/', views.registration),
+    path('dashboard/', views.dashboard),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile_management),
     path('payment/', views.payment)
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect or show success
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/registration.html', {'form': form})
 
-def login(request):
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
     return render(request, 'users/login.html')
 
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
+
+#@login_required
 def profile_management(request):
     return render(request, 'users/profile.html')
 
+@login_required
 def payment(request):
     return render(request, 'users/payment.html')
 

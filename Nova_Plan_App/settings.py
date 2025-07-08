e01a741876c4a583f1cf0d62b834743d02cad900
Francklin509 <162865584+Francklin509@users.noreply.github.com>
2025-07-08 07:09:13 +0000

    12

--- Nova_Plan_App/settings.py
+++ Nova_Plan_App/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-jp!n#4mq!oaq=0&+_k$$_5aj-!d6j9g-@l-%ivp4=&ikfjl1ty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nova-plan-builders.onrender.com', 'localhost']
 
 # Application definition
 

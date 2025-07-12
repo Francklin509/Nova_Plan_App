75946a7d3ff467abf3d07b89ada91f1074eb26a4
Francklin509 <162865584+Francklin509@users.noreply.github.com>
2025-07-12 07:37:02 +0000

    error500

--- Nova_Plan_App/settings.py
+++ Nova_Plan_App/settings.py
@@ -29,9 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jp!n#4mq!oaq=0&+_k$$_5aj-!d6j9g-@l-%ivp4=&ikfjl1ty'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['nova-plan-builders.onrender.com']
+DEBUG = False
+ALLOWED_HOSTS = ['nova-plan-builders.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 

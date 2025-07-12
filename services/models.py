1d4ed1aa8205d9480a2146779087993003f89084
Francklin509 <162865584+Francklin509@users.noreply.github.com>
2025-07-12 08:10:16 +0000

    value

--- services/models.py
+++ services/models.py
@@ -4,6 +4,15 @@ from django.db import models
 class ArchitecturePost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    tags = models.CharField(max_length=200, blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+    comments_count = models.PositiveIntegerField(default=0)
+    aera = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to='architecture/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
